88c762cc064b9a6190f17093b653d3d46c7b5604
Joel Yourstone <joel.yourstone@gmail.com>
2016-07-01 18:14:24 +0200

    Added page to download the app

---
+++ api/webroot/index.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<?php
+
+if(isset($_POST['auteh'])) {
+    if($_POST['auteh'] == "danielharensvans") {
+        setcookie("auth", "345g6g4363465g", time()+3600 * 24 * 30);
+        header("Location: .");
+    }
+}
+
+
+if(!isset($_COOKIE['auth']) || $_COOKIE['auth'] != "345g6g4363465g") {
+    
+    ?>
+<form method="post">
+    <input type="text" name="auteh">
+    <input type="submit" value="Send">
+</form>
+    <?php
+} else {
+    ?>
+<a href="#">Ladda ned här</a>
+    <?php
+}
+
+?>
