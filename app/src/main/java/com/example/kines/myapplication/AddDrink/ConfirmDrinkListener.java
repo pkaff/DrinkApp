fdf56646b0e2a82ce65a698ebb3ed7174355c22f
pkaff <wishing@live.se>
2016-12-11 22:39:29 +0100

    Add confirm drink listener

---
+++ app/src/main/java/com/example/kines/myapplication/AddDrink/ConfirmDrinkListener.java
@@ -0,0 +1,19 @@
+package com.example.kines.myapplication.AddDrink;
+
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.kines.myapplication.AddDrinkActivity;
+import com.example.kines.myapplication.Ingredient;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kines on 2016-09-26.
+ */
+
+public class ConfirmDrinkListener implements DialogInterface.OnClickListener {
+
+}
